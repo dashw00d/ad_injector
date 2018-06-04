de57d5d16b752e66d8c3be0324d9c68166f74ee6
Ryan Stefan <letmegoblind@gmail.com>
2018-06-04 09:09:56 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -34,6 +34,7 @@ function addParagraphs($pcontent) {
 		if (array_intersect($atag_ids, $ptag_ids)) {
 			$ad_paragraph_after = get_post_meta( $ad_id, 'ad_paragraph_after', true);
 			$url = wp_get_attachment_url( get_post_thumbnail_id($ad_id) );
+			break;
 
 	}
 
